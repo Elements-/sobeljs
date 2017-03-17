da9b890ebd9fff44a6b607f5e0e3496c7a334804
root <cam@elmnts.co>
2017-03-17 11:14:45 -0400

    hough transform implementation (WIP)

--- index.js
+++ index.js
@@ -1,37 +1,65 @@
 var fs = require('fs');
 var pngjs = require('pngjs');
 var glur = require('glur');
-var sobel = require('./lib/sobel');
+var filters = require('./lib');
+var kernels = require('./kernels');
 
-var default_kernels = [
-  [
-    [-1, 0, 1],
-    [-2, 0, 2],
-    [-1, 0, 1]
-  ],
-  [
-    [-1, -2, -1],
-    [ 0,  0,  0],
-    [ 1,  2,  1]
-  ]
-];
+console.time('read');
+var png_raw = fs.readFileSync(process.argv[2]);
+console.timeEnd('read');
 
-console.log('Reading image');
-var png = pngjs.PNG.sync.read(
-  fs.readFileSync(process.argv[2])
-);
 
-console.log('Blurring image');
+console.time('decode');
+var png = pngjs.PNG.sync.read(png_raw);
+console.timeEnd('decode');
+
+
+console.time('blur');
 glur(png.data, png.width, png.height, 1);
+console.timeEnd('blur');
 
-console.log('Processing image');
-png = sobel(
-  png,
-  default_kernels
-);
 
-console.log('Writing image');
-fs.writeFileSync(
-  process.argv[3],
-  pngjs.PNG.sync.write(png)
+console.time('rgba to greyscale');
+var greyscale_data = filters.greyscale.fromRGBA(png.data);
+console.timeEnd('rgba to greyscale');
+
+
+console.time('sobel');
+var sobel_data = filters.sobel(
+  {
+    width: png.width,
+    height: png.height,
+    data: greyscale_data
+  },
+  [
+    kernels.X,
+    kernels.Y
+  ]
 );
+console.timeEnd('sobel');
+
+console.time('lines');
+var lines_data = filters.lines(
+  {
+    width: png.width,
+    height: png.height,
+    data: sobel_data,
+    threshold: 200
+  }
+);
+console.timeEnd('lines');
+
+
+console.time('greyscale to rgba');
+png.data = filters.greyscale.toRGBA(lines_data);
+console.timeEnd('greyscale to rgba');
+
+
+console.time('encode');
+var out_png = pngjs.PNG.sync.write(png);
+console.time('encode');
+
+
+console.time('write');
+fs.writeFileSync(process.argv[3], out_png);
+console.timeEnd('write');
